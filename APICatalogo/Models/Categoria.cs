f1ca77680ab8041eaf82eaca0673544fe62df477
JOSE PORDEUS <jose.pordeus@senaisp.edu.br>
2020-09-16 21:33:42 -0300

    Create Method Context

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,15 @@ namespace APICatalogo.Models
 {
     public class Categoria
     {
+
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
         public int CategoriaId { get; set; }
         public string Nome { get; set; }
         public string ImagemUrl { get; set; }
+
+        public ICollection<Produto> Produtos { get; set; }
     }
 }
--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -10,9 +10,12 @@ namespace APICatalogo.Models
         public int ProdutoId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public string Preco { get; set; }
+        public decimal Preco { get; set; }
         public string ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
+
+        public Categoria Categoria { get; set; }
+        public int CategoriaId { get; set; }
     }
 }
