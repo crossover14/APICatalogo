67a7fa3a26a97d0b7c0b223884115cfc03be80c1
LeandroVPordeus <58612986+LeandroVPordeus@users.noreply.github.com>
2020-09-26 00:40:28 -0300

    Implementacao Logger

---
+++ APICatalogo/Logging/CustomLoggerProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Logging
+{
+    public class CustomLoggerProvider : ILoggerProvider
+    {
+        readonly CustomLoggerProviderConfiguration loggerConfig;
+
+        readonly ConcurrentDictionary<string, CustomerLogger> loggers =
+                 new ConcurrentDictionary<string, CustomerLogger>();
+
+        public CustomLoggerProvider(CustomLoggerProviderConfiguration config)
+        {
+            loggerConfig = config;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return loggers.GetOrAdd(categoryName, name => new CustomerLogger(name, loggerConfig));
+        }
+
+        public void Dispose()
+        {
+            loggers.Clear();
+        }
+    }
+}
